f96a3d2cb90a69ce757ba50f0914c05246e24b23
kevelscy <kevelscy@gmail.com>
2022-01-21 12:47:26 -0400

    test: deploy

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,5 @@
-// import server from './server'
+import server from './server'
 
-// server.listen(server.get('PORT'), () => {
-//   console.log(`Listening in the port ${server.get('PORT')}`)
-// })
-
-import express from 'express'
-const server = express()
-
-server.get('/', (req, res) => {
-  return res.status(200).json({
-    data: 'Epale',
-    error: null
-  })
-})
-
-server.listen(process.env.PORT || 4000, () => {
-  console.log('Listening in the port 4000')
+server.listen(server.get('PORT'), () => {
+  console.log(`Listening in the port ${server.get('PORT')}`)
 })
