395d10516766e668c90a9280427d7307d8c5463a
kevelscy <kevelscy@gmail.com>
2022-01-21 12:37:13 -0400

    test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
-import server from './server'
+// import server from './server'
 
-server.listen(server.get('PORT'), () => {
-  console.log(`Listening in the port ${server.get('PORT')}`)
+// server.listen(server.get('PORT'), () => {
+//   console.log(`Listening in the port ${server.get('PORT')}`)
+// })
+
+import express from 'express'
+const server = express()
+
+server.use('/', (req, res) => {
+  return res.status(200).json({
+    data: 'Epale',
+    error: null
+  })
+})
+
+server.listen(4000, () => {
+  console.log('Listening in the port 4000')
 })
