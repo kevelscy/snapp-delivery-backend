c46160a673541e481411f71ac60b2a5444f973f7
kevelscy <kevelscy@gmail.com>
2022-01-21 12:58:48 -0400

    test: deploy

--- dist/index.js
+++ dist/index.js
@@ -3,8 +3,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = __importDefault(require("./server"));
-server_1.default.listen(server_1.default.get('PORT'), () => {
-    console.log(`Listening in the port ${server_1.default.get('PORT')}`);
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const routes_1 = require("./routes");
+const router = express_1.default.Router();
+const server = (0, express_1.default)();
+dotenv_1.default.config();
+const RootRouter = router.get('/', (req, res) => {
+    return res.status(200).json({
+        data: 'epale',
+        error: null
+    });
+});
+server.use('/api', routes_1.SignInRoutes);
+server.use('/api', routes_1.SignUpRoutes);
+server.use('/api', routes_1.RefreshTokenRoutes);
+server.use('/api', routes_1.UsersRoutes);
+server.use(RootRouter);
+server.use(routes_1.NotFound);
+server.use(routes_1.HandleErrors);
+server.listen(process.env.PORT || 4000, () => {
+    console.log('Listeting in the port' + process.env.PORT || 4000);
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,14 @@
 
 import express from 'express'
 import dotenv from 'dotenv'
+import {
+  HandleErrors,
+  NotFound,
+  SignInRoutes,
+  SignUpRoutes,
+  RefreshTokenRoutes,
+  UsersRoutes
+} from './routes'
 
 const router = express.Router()
 const server = express()
@@ -18,15 +26,13 @@ const RootRouter = router.get('/', (req, res) => {
   })
 })
 
-const SignInRouter = router.get('/signin', (req, res) => {
-  return res.status(200).json({
-    data: 'signin',
-    error: null
-  })
-})
-
-server.use('/api', SignInRouter)
+server.use('/api', SignInRoutes)
+server.use('/api', SignUpRoutes)
+server.use('/api', RefreshTokenRoutes)
+server.use('/api', UsersRoutes)
 server.use(RootRouter)
+server.use(NotFound)
+server.use(HandleErrors)
 
 server.listen(process.env.PORT || 4000, () => {
   console.log('Listeting in the port' + process.env.PORT || 4000)
